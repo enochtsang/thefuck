61bdbd3496d3c7cc91950c28d74cc711fac42e99
Enoch Tsang <etsang1@hotmail.com>
2016-04-10 22:27:05 -0600

    client now send tuple of strings

--- thefuck/communication/sender.py
+++ thefuck/communication/sender.py
@@ -3,13 +3,18 @@ import pickle
 from thefuck import logs
 from . import receiver
 
+# SERVER_IP = "162.246.157.118"
+# SERVER_PORT = 9001
 
 SERVER_IP = "localhost"
-SERVER_PORT = 3033
+SERVER_PORT = 3000
 
 def send_command(to_be_corrected):
     client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect((SERVER_IP, SERVER_PORT))
     logs.debug("Sending command")
-    client_socket.send(pickle.dumps(to_be_corrected))
+    script = to_be_corrected.script
+    stdout = to_be_corrected.stdout
+    stderr = to_be_corrected.stderr
+    client_socket.send(pickle.dumps((script, stdout, stderr)))
     return receiver.receive_from_server(client_socket)
