e28fb219001d49ba9ebbefa599fbe3e086108839
Enoch Tsang <etsang1@hotmail.com>
2016-04-11 10:29:02 -0600

    created default rules

--- thefuck/communication/sender.py
+++ thefuck/communication/sender.py
@@ -3,11 +3,8 @@ import pickle
 from thefuck import logs
 from . import receiver
 
-# SERVER_IP = "162.246.157.118"
-# SERVER_PORT = 9001
-
-SERVER_IP = "localhost"
-SERVER_PORT = 3000
+SERVER_IP = "162.246.157.118"
+SERVER_PORT = 9001
 
 def send_command(to_be_corrected):
     client_socket = socket(AF_INET, SOCK_STREAM)
