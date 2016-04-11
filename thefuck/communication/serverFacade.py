92724ea7a10c7a5301398c5e2ecb3c100de3b3f7
jeinglis <jeinglis@ucalgary.ca>
2016-04-10 18:55:15 -0600

    added ping

--- thefuck/communication/serverFacade.py
+++ thefuck/communication/serverFacade.py
@@ -1,9 +1,13 @@
 from .sender import send_command
+import os
 
+SERVER_IP = "162.246.157.118"
 
 def send_to_server(to_be_corrected):
-    Sender.send_command(to_be_corrected)
+    response = os.system("ping -c 1 " + hostname)
+    if response == 0:
+        return Sender.send_command(to_be_corrected)
+    else
+        return None
 
-def receive_corrected(corrected):
-    return corrected
 
